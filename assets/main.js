fc2af48db08fd1e1e9e3ca5250e0718fb9361487
dandanshan@Mac17010.local <dandanshan@cw.com.tw>
2020-04-29 12:29:39 +0800

    Add wave and popup & visual optimize

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+	headerStyle();
+	waveShow();
+	goTopShow();
+
 	$('.burger').click(function () {
 		$(this).toggleClass('active');
 		$('.nav').toggleClass('active');
@@ -9,39 +13,6 @@ $(document).ready(function () {
 		$('html, body').animate({
 			scrollTop: 0
 		}, 1000);
-	});
-
-	$(window).scroll(function () {
-		//header position
-		$(window).scrollTop() > 0 ? $('.header').addClass('active') : $('.header').removeClass('active');
-		//header background color
-		$(window).scrollTop() > $('#home').outerHeight() ? $('.header').css('background', '#254460') : $('.header').css('background', 'transparent');
-
-		//go top show
-		if ($(this).scrollTop() > 800) {
-			$('#goTop, .gotop').addClass('active');
-		} else {
-			$('#goTop, .gotop').removeClass('active');
-		}
-
-		var $target = $('#logotype .scroll');
-		var index = 0;
-		var length = $target.length;
-
-		for (index; index < length; index++) {
-			var pos = window.pageYOffset * $target[index].dataset.rate;
-
-			if ($target[index].dataset.direction === 'vertical') {
-				$target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
-
-			} else {
-
-				var posX = window.pageYOffset * $target[index].dataset.ratex;
-				var posY = window.pageYOffset * $target[index].dataset.ratey;
-
-				$target[index].style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)';
-			}
-		}
 	});
 
 	$('.slick--video').slick({
@@ -141,8 +112,6 @@ $(document).ready(function () {
 		]
 	});
 
-
-
 	//yt video overlay
 	$('.slick--video .slick__item').click(function (e) {
 		var url = $(this).find('img').data('url');
@@ -152,4 +121,67 @@ $(document).ready(function () {
 			ytSrc: url,
 		})
 	});
+
+	vex.defaultOptions.className = 'vex-theme-wireframe';
+	vex.dialog.defaultOptions.showCloseButton = true;
+
+	$('.wave').click(function() {
+		vex.dialog.alert({
+			message: '台灣讓人安心，是很多人的努力面對未來，邀請你串連正面的力量，為台灣做一件事!',
+			input: '<div class="dialog__content"><ul class="grid grid--wave"><li><div><label>響應方法 A</label><ul><li>step1：在 A4 紙上寫下「#安心台灣運動，我願意__ 」</li><li>step2：手拿宣言錄一段15秒影片，或拍一張照片</li><li>step3：上傳影片/照片到FB或IG，並@好友接力響應</li></ul></div></li><li><label>響應方法 B</label><p>在網站上留下您的宣言，與大家分享！</p><a href="#motion" class="btn btn--block btn--dark icon icon--a icon__more icon__dark"s"><span class="btn__text">立即響應</span></a></li></ul></div>',
+		})
+	});
+
+	$(window).scroll(function () {
+
+		headerStyle();
+		waveShow();
+		goTopShow();
+
+		var $target = $('#logotype .scroll');
+		var index = 0;
+		var length = $target.length;
+
+		for (index; index < length; index++) {
+			var pos = window.pageYOffset * $target[index].dataset.rate;
+
+			if ($target[index].dataset.direction === 'vertical') {
+				$target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
+
+			} else {
+
+				var posX = window.pageYOffset * $target[index].dataset.ratex;
+				var posY = window.pageYOffset * $target[index].dataset.ratey;
+
+				$target[index].style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)';
+			}
+		}
+	});
+
+	//header style
+    function headerStyle() {
+		//header position
+		$(window).scrollTop() > 0 ? $('.header').addClass('active') : $('.header').removeClass('active');
+		//header background color
+		$(window).scrollTop() > $('#home').outerHeight() ? $('.header').css('background', '#254460') : $('.header').css('background', 'transparent');
+	}
+
+	//wave show
+    function waveShow() {
+		if ($(window).scrollTop() > ($(window).outerHeight()*0.7)) {
+			$('.wave').fadeIn('slow');
+		} else {
+			$('.wave').fadeOut('slow');
+		}
+	}
+
+	//go top show
+	function goTopShow() {
+        if ($(window).scrollTop() > 800) {
+			$('#goTop, .gotop').addClass('active');
+		} else {
+			$('#goTop, .gotop').removeClass('active');
+		}
+    }
+
 });
