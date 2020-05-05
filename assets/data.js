1adefb4cdacad3288fd10a847b82558b8b2383d2
dandanshan@Mac17010.local <dandanshan@cw.com.tw>
2020-05-05 18:11:44 +0800

    Update video and remove data sort

--- assets/data.js
+++ assets/data.js
@@ -1,258 +1,212 @@
 data = [
 	{
-		"sort": "1",
 		"code": "qmAfcVKFOJQ",
 	},
 	{
-		"sort": "2",
+		"code": "sYwoJxtYOrw",
+	},
+	{
 		"code": "9C4lHOvnj7k",
 	},
 	{
-		"sort": "3",
 		"code": "0D5_jQEcyPM",
 	},
 	{
-		"sort": "4",
 		"code": "FBeLD8ijC2o",
 	},
 	{
-		"sort": "5",
 		"code": "seHVPULIZ6k",
 	},
 	{
-		"sort": "6",
 		"code": "1I7iT-yefYk",
 	},
 	{
-		"sort": "7",
 		"code": "2VTFVl_vnsE",
 	},
 	{
-		"sort": "8",
 		"code": "OZ9KuozC2rM",
 	},
 	{
-		"sort": "9",
 		"code": "8LrmoX9Fwtc",
 	},
 	{
-		"sort": "10",
-		"code": "_20F6HOKt10",
+		"code": "3_Hn_THLAdQ",
 	},
 	{
-		"sort": "11",
+		"code": "lfCwQLId4Y0",
+	},
+	{
 		"code": "J7H9OcuhfQ0",
 	},
 	{
-		"sort": "12",
 		"code": "1Ik6FvXgslQ",
 	},
 	{
-		"sort": "13",
 		"code": "4dfywaHiu9s",
 	},
 	{
-		"sort": "14",
 		"code": "zoVJeXFxCYo",
 	},
 	{
-		"sort": "15",
 		"code": "gtMJWf1DFVw",
 	},
 	{
-		"sort": "16",
 		"code": "pHmRehZCK-k",
 	},
 	{
-		"sort": "17",
 		"code": "I00FowuZZvc",
 	},
 	{
-		"sort": "18",
 		"code": "mSmxcUxuzPI",
 	},
 	{
-		"sort": "19",
 		"code": "PANV05qPFyQ",
 	},
 	{
-		"sort": "20",
+		"code": "mXRUFbs0bio",
+	},
+	{
+		"code": "TDBDtJOlP4w",
+	},
+	{
+		"code": "lPkKTfLmqL8",
+	},
+	{
 		"code": "2sC2dkbJ0EQ",
 	},
 	{
-		"sort": "21",
 		"code": "_f5TOTMBTOM",
 	},
 	{
-		"sort": "22",
+		"code": "N11drQNpFPM",
+	},
+	{
 		"code": "W5eW7cnaAJ8",
 	},
 	{
-		"sort": "23",
 		"code": "0dHuhiHOE4k",
 	},
 	{
-		"sort": "24",
 		"code": "epDEdKbfmfQ",
 	},
 	{
-		"sort": "25",
 		"code": "wijAH8oHooE",
 	},
 	{
-		"sort": "26",
 		"code": "kZULxWwUcmE",
 	},
 	{
-		"sort": "27",
 		"code": "EKAwYGJGt7o",
 	},
 	{
-		"sort": "28",
 		"code": "ySbfbV6ktGE",
 	},
 	{
-		"sort": "29",
 		"code": "k8VzJ3OhCTo",
 	},
 	{
-		"sort": "30",
 		"code": "SO01FI-3eWU",
 	},
 	{
-		"sort": "31",
 		"code": "B9pvrmiC0Sk",
 	},
 	{
-		"sort": "32",
 		"code": "I1m_WrKQkxU",
 	},
 	{
-		"sort": "33",
 		"code": "gPFBYYTYbpA",
 	},
 	{
-		"sort": "34",
 		"code": "qM6FY9nd2NM",
 	},
 	{
-		"sort": "35",
 		"code": "Dty7LtnAMCo",
 	},
 	{
-		"sort": "36",
 		"code": "TrFeMBpaJ3s",
 	},
 	{
-		"sort": "37",
 		"code": "AQgO-TQaGAk",
 	},
 	{
-		"sort": "38",
 		"code": "etoTB6KSJhM",
 	},
 	{
-		"sort": "39",
 		"code": "Y4tTtGaosfI",
 	},
 	{
-		"sort": "40",
 		"code": "Q-Xo2zZ5wXk",
 	},
 	{
-		"sort": "41",
 		"code": "Elh-QNoTQFU",
 	},
 	{
-		"sort": "42",
 		"code": "EF2tAUXOjIQ",
 	},
 	{
-		"sort": "43",
 		"code": "O1adYZy0qOM",
 	},
 	{
-		"sort": "44",
 		"code": "Pi3gjfXATMg",
 	},
 	{
-		"sort": "45",
 		"code": "Bt_nA6hE6KE",
 	},
 	{
-		"sort": "46",
 		"code": "TXlOTaDWecw",
 	},
 	{
-		"sort": "47",
 		"code": "bvmsZiMi1Bo",
 	},
 	{
-		"sort": "48",
 		"code": "B9pvrmiC0Sk",
 	},
 	{
-		"sort": "49",
 		"code": "CTl9D4sZWLg",
 	},
 	{
-		"sort": "50",
 		"code": "XN322YDOChY",
 	},
 	{
-		"sort": "51",
 		"code": "-GJ-II4nZbE",
 	},
 	{
-		"sort": "52",
 		"code": "vYuq_GJQ3n4",
 	},
 	{
-		"sort": "53",
 		"code": "4CxwlGms_xg",
 	},
 	{
-		"sort": "54",
 		"code": "wqIyoI4aOVA",
 	},
 	{
-		"sort": "55",
 		"code": "d2QJte0SyOY",
 	},
 	{
-		"sort": "56",
 		"code": "0cN2NmLloBo",
 	},
 	{
-		"sort": "57",
 		"code": "s5wRFSwbxLk",
 	},
 	{
-		"sort": "58",
 		"code": "mqlQISonIYo",
 	},
 	{
-		"sort": "59",
 		"code": "KECYnwMP0P8",
 	},
 	{
-		"sort": "60",
 		"code": "EjgPfwUqC_g",
 	},
 	{
-		"sort": "61",
 		"code": "FV6zZTAnC3k",
 	},
 	{
-		"sort": "62",
 		"code": "cnFTA1xmjxE",
 	},
 	{
-		"sort": "63",
 		"code": "VQXC-BbDujI",
 	},
 	{
-		"sort": "64",
 		"code": "KMHfRkVIYi8",
 	},
 	// {
@@ -260,183 +214,165 @@ data = [
 	// 	"code": "nxQlCPPNJ9A",
 	// },
 	{
-		"sort": "66",
 		"code": "teoz4NJx2l4",
 	},
 	{
-		"sort": "67",
 		"code": "mIP0NQlL8Jw",
 	},
 	{
-		"sort": "68",
 		"code": "av-teby0B8k",
 	},
 	{
-		"sort": "69",
 		"code": "ldCTw0Jy42c",
 	},
 	{
-		"sort": "70",
 		"code": "9D52E-dXl4o",
 	},
 	{
-		"sort": "71",
 		"code": "bsOlFkSshA4",
 	},
 	{
-		"sort": "72",
 		"code": "MqhRD1IJXbM",
 	},
 	{
-		"sort": "73",
 		"code": "oc1R7aE0Ot0",
 	},
 	{
-		"sort": "74",
 		"code": "nkmaSHIZBdE",
 	},
 	{
-		"sort": "75",
 		"code": "IqfhK-J-oRk",
 	},
 	{
-		"sort": "76",
 		"code": "nisYPepmMAM",
 	},
 	{
-		"sort": "77",
 		"code": "BTQciGm29vo",
 	},
 	{
-		"sort": "78",
 		"code": "Dw4y_54PIfQ",
 	},
 	{
-		"sort": "79",
 		"code": "js3nUNWEuGg",
 	},
 	{
-		"sort": "80",
 		"code": "KSULw8BtLx0",
 	},
 	{
-		"sort": "81",
 		"code": "1ISFyebmTzI",
 	},
 	{
-		"sort": "82",
 		"code": "VyOd4-K5bwU",
 	},
 	{
-		"sort": "83",
 		"code": "p5o5j0q2iDc",
 	},
 	{
-		"sort": "84",
 		"code": "QeR_bcAdFK8",
 	},
 	{
-		"sort": "85",
 		"code": "kQYhy5Ftyww",
 	},
 	{
-		"sort": "86",
 		"code": "5uZw5yXCD54",
 	},
 	{
-		"sort": "87",
 		"code": "xF-n_xKAnS0",
 	},
 	{
-		"sort": "88",
 		"code": "LYWf6Lot1R0",
 	},
 	{
-		"sort": "89",
 		"code": "k4WbxQlQuU0",
 	},
 	{
-		"sort": "90",
 		"code": "qVG6xS4qzLE",
 	},
 	{
-		"sort": "91",
 		"code": "Bk7r-DQGohI",
 	},
 	{
-		"sort": "92",
 		"code": "QcghY9N82qY",
 	},
 	{
-		"sort": "93",
 		"code": "Opj0jndGZD8",
 	},
 	{
-		"sort": "94",
 		"code": "GiSC1BeztFs",
 	},
 	{
-		"sort": "95",
 		"code": "r3MWG1FkT9c",
 	},
 	{
-		"sort": "96",
 		"code": "3J3p14z-1bI",
 	},
 	{
-		"sort": "97",
 		"code": "uB1Rn1ia7uw",
 	},
 	{
-		"sort": "98",
 		"code": "IVmKqlycoVQ",
 	},
 	{
-		"sort": "99",
 		"code": "JtfC738VWKc",
 	},
 	{
-		"sort": "100",
 		"code": "GQOAb-HUEMU",
 	},
 	{
-		"sort": "101",
 		"code": "LaqVdyzr1zA",
 	},
 	{
-		"sort": "102",
 		"code": "JPfWTQW4Bg8",
 	},
 	{
-		"sort": "103",
 		"code": "0E3yKSWTHpg",
 	},
 	{
-		"sort": "104",
 		"code": "1nVRIGyglUc",
 	},
 	{
-		"sort": "105",
 		"code": "MuOCosx_7IY",
 	},
 	{
-		"sort": "106",
 		"code": "BJnkX0oMQvk",
 	},
 	{
-		"sort": "107",
 		"code": "MuiaPYKKFVw",
 	},
 	{
-		"sort": "108",
 		"code": "5b7VyTQjWTo",
 	},
 	{
-		"sort": "109",
 		"code": "CTl9D4sZWLg",
 	},
 	{
-		"sort": "110",
 		"code": "F2pU3m227fQ",
-	}
+	},
+	{
+		"code": "epwqQitUlBI",
+	},
+	{
+		"code": "DCpTyliPaWs",
+	},
+	{
+		"code": "_8O4ASToQik",
+	},
+	{
+		"code": "JeQ9IYHZrRk",
+	},
+	{
+		"code": "v06WmmuKWtI",
+	},
+	{
+		"code": "0C4BAXbZgpE",
+	},
+	{
+		"code": "knOui6RrizQ",
+	},
+	{
+		"code": "F-oPL9IFO1c",
+	},
+	{
+		"code": "I1i2ho9Zy9A",
+	},
 ]
